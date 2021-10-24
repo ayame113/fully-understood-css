2be030ca59e0612913a130335d23e8571e12222c
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-10-25 00:02:15 +0900

    css

--- listener.ts
+++ listener.ts
@@ -8,18 +8,18 @@ try {
 
 interface Listener {
   pattern: URLPattern;
-  handler: (
+  handler: ((
     { request, url }: { request: Readonly<Request>; url: Readonly<URL> },
-  ) => Response;
+  ) => Response | Promise<Response>);
 }
 
 export const listeners: Listener[] = [
   {
     pattern: new URLPattern({ pathname: "/" }),
-    handler: ({ request, url }) => {
+    handler: async ({ request, url }) => {
       console.log(request, url);
       return new Response(
-        'Hello World from <a href="https://github.com/ayame113/deno_deploy_template">ayame113/deno_deploy_template</a> !',
+        await Deno.readFile(new URL("./static/index.html", import.meta.url)),
         {
           headers: {
             "Content-Type": "text/html; charset=utf-8",
@@ -28,28 +28,4 @@ export const listeners: Listener[] = [
       );
     },
   },
-  /*{
-    // Process the request when this pattern is matched.
-    // See also URLPattern API:
-    // https://developer.mozilla.org/en-US/docs/Web/API/URLPattern
-
-    pattern: new URLPattern({ pathname: "/foobar/:id" }),
-
-    // Handle the request.
-    // - request: Request object.
-    // - url: URL object (requested url).
-    // should return the Response object.
-
-    handler: ({ request, url }) => {
-      console.log(request, url);
-      return new Response(
-        'Hello World from <a href="https://github.com/ayame113/deno_deploy_template">ayame113/deno_deploy_template</a> !',
-        {
-          headers: {
-            "Content-Type": "text/html; charset=utf-8",
-          },
-        },
-      );
-    },
-  },*/
 ];
