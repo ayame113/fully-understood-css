433a61eef4ccd15a581811bdc05e6867e0759b82
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-04-01 18:53:13 +0900

    Update typecheck.ts (#18)
    * Update typecheck.ts
    
    * Update ci.yml

--- dev/typecheck.ts
+++ dev/typecheck.ts
@@ -1,4 +1,3 @@
-import type {} from "https://deno.land/x/deploy@0.5.0/types/deploy.ns.d.ts";
-import type {} from "https://deno.land/x/deploy@0.5.0/types/deploy.window.d.ts";
-import type {} from "https://deno.land/x/deploy@0.5.0/types/deploy.fetchevent.d.ts";
+// deno-lint-ignore-file
+import type {} from "https://deno.land/x/deploy_type_checker@v0.0.4/dts/deno.window.d.ts";
 import "../serve.ts";
